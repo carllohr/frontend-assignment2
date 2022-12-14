1779fcd2d3a70c1404c2b163a5b17d58aa297a44
carllohr <calle.lohr@gmail.com>
2022-12-14 14:22:17 +0100

    init todolist

---
+++ script.js
@@ -0,0 +1,47 @@
+const addBtn = document.querySelector('#addBtn');
+
+const element = document.getElementById('datePicker');
+element.valueAsNumber = Date.now()-(new Date()).getTimezoneOffset()*60000;
+
+function addActivity()
+{
+    
+    let inputActivity = document.getElementById("inputActivity").value;
+    let inputCategory = document.getElementById("inputCategory").value;
+    let activityListEl = document.getElementById("a-content");
+
+    
+
+    let activityListItem = document.createElement("div");
+    activityListItem.classList.add("activity-list-item");
+    activityListEl.appendChild(activityListItem);
+
+    let removeBtn = document.createElement("button");
+    removeBtn.innerHTML = "Remove";    
+    removeBtn.addEventListener("click", function(){
+        this.parentNode.remove();
+    })
+
+
+    let activityListItemName = document.createElement("span");
+    activityListItemName.innerText = inputActivity;
+    let activityListItemDate = document.createElement("span");
+    activityListItemDate.innerText = element.value;
+    let activityListItemCategory = document.createElement("span");
+    activityListItemCategory.innerText = inputCategory;
+
+    activityListItem.appendChild(activityListItemName);
+    activityListItem.appendChild(activityListItemDate);
+    activityListItem.appendChild(activityListItemCategory);
+    activityListItem.appendChild(removeBtn);
+   
+    
+
+}
+
+function removeActivity(){
+
+}
+
+
+
